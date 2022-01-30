c927ad2b6436b64e14eee2f47a9606404d6aea7b
Armend Shala <armend.shala@telekom.de>
2022-01-30 21:16:31 +0100

    Create design

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let seconds = 0;
+let hours = 0;
+let minutes = 0;
+
+const max = 360;
+
+// setInterval(() => {
+//   document.getElementById("seconds").style.transform = `rotate(${seconds}deg)`;
+//   seconds = seconds + 1;
+
+//   if (seconds > max) {
+//     seconds = 0;
+//   }
+// }, 100);
+
+// setInterval(() => {
+//   document.getElementById("minutes").style.transform = `rotate(${minutes}deg)`;
+//   minutes = minutes + 1;
+
+//   if (minutes > max) {
+//     minutes = 0;
+//   }
+// }, 1000);
+
+// setInterval(() => {
+//   document.getElementById("hours").style.transform = `rotate(${hours}deg)`;
+//   hours = hours + 1;
+
+//   if (hours > max) {
+//     hours = 0;
+//   }
+// }, 10000);
