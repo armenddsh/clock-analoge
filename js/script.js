6af4118225ed53af071bfe94f8314142eee2f47f
Armend Shala <armend.shala@telekom.de>
2022-01-31 00:13:38 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -4,29 +4,34 @@ let minutes = 0;
 
 const max = 360;
 
-// setInterval(() => {
-//   document.getElementById("seconds").style.transform = `rotate(${seconds}deg)`;
-//   seconds = seconds + 1;
+setInterval(() => {
+    const date = new Date();
+    let day = date.getDate();          // Get the day as a number (1-31)
+    let weekDayNum = date.getDay();           // Get the weekday as a number (0-6)
+    let year = date.getFullYear();      // Get the four digit year (yyyy)
+    let hours = date.getHours();         // Get the hour (0-23)
+    let minutes = date.getMinutes();       // Get the minutes (0-59)
+    let month = date.getMonth();         // Get the month (0-11)
+    let seconds = date.getSeconds();       // Get the seconds (0-59)
 
-//   if (seconds > max) {
-//     seconds = 0;
-//   }
-// }, 100);
+    const k = 6;
+    const hour = 12;
+    const deg = 90;
 
-// setInterval(() => {
-//   document.getElementById("minutes").style.transform = `rotate(${minutes}deg)`;
-//   minutes = minutes + 1;
+    if (hours >= 12) {
+        hours = hours - hour;
+    }
 
-//   if (minutes > max) {
-//     minutes = 0;
-//   }
-// }, 1000);
+    seconds = seconds * k;
+    minutes = minutes * k;
+    hours = hours * 4 * k;
 
-// setInterval(() => {
-//   document.getElementById("hours").style.transform = `rotate(${hours}deg)`;
-//   hours = hours + 1;
+    minutes = minutes + deg;
+    seconds = seconds + deg;
+    hours = hours + deg;
 
-//   if (hours > max) {
-//     hours = 0;
-//   }
-// }, 10000);
+    document.getElementById("seconds").style.transform = `rotate(${seconds}deg)`;
+    document.getElementById("minutes").style.transform = `rotate(${minutes}deg)`;
+    document.getElementById("hours").style.transform = `rotate(${hours}deg)`;
+
+}, 500);
