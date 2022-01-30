60cea3ca3211e3dce46df1974da9773b0ea81cdf
Armend Shala <armend.shala@telekom.de>
2022-01-31 00:42:21 +0100

    Bug fix and improvement

--- js/script.js
+++ js/script.js
@@ -6,12 +6,8 @@ const max = 360;
 
 setInterval(() => {
     const date = new Date();
-    let day = date.getDate();          // Get the day as a number (1-31)
-    let weekDayNum = date.getDay();           // Get the weekday as a number (0-6)
-    let year = date.getFullYear();      // Get the four digit year (yyyy)
     let hours = date.getHours();         // Get the hour (0-23)
     let minutes = date.getMinutes();       // Get the minutes (0-59)
-    let month = date.getMonth();         // Get the month (0-11)
     let seconds = date.getSeconds();       // Get the seconds (0-59)
 
     const k = 6;
@@ -20,18 +16,20 @@ setInterval(() => {
 
     if (hours >= 12) {
         hours = hours - hour;
+    } else {
+        hours = hours + 0;
     }
 
     seconds = seconds * k;
     minutes = minutes * k;
-    hours = hours * 4 * k;
+    hours = hours * 5 * k;
 
-    minutes = minutes + deg;
-    seconds = seconds + deg;
-    hours = hours + deg;
+    const minutesDeg = minutes + deg;
+    const secondsDeg = seconds + deg;
+    const hoursDeg = ( hours + ( minutes / hour ) ) + deg;
 
-    document.getElementById("seconds").style.transform = `rotate(${seconds}deg)`;
-    document.getElementById("minutes").style.transform = `rotate(${minutes}deg)`;
-    document.getElementById("hours").style.transform = `rotate(${hours}deg)`;
+    document.getElementById("seconds").style.transform = `rotate(${secondsDeg}deg)`;
+    document.getElementById("minutes").style.transform = `rotate(${minutesDeg}deg)`;
+    document.getElementById("hours").style.transform = `rotate(${hoursDeg}deg)`;
 
 }, 500);
